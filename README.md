7469d1eb7fbf23cd08c2919bc21df20440e5de9d
etojeDenys <denysdenart@gmail.com>
2024-04-01 16:47:06 +0300

    fix firebase deploy

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 Provide your Firebase info in the
 `.env` file:
 ````
+
 REACT_APP_APIKEY='...'
 REACT_APP_AUTHDOMAIN='...'
-REACT_APP_PROJECTID='...'
+REACT_APP_PROJECT_ID='...'
 REACT_APP_STORAGEBUCKET='...'
 REACT_APP_MESSAGINGSENDERID='...'
 REACT_APP_APPID='...'
+
 ````
--- src/firebase.ts
+++ src/firebase.ts
@@ -6,7 +6,7 @@ import { getStorage } from 'firebase/storage';
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_APIKEY,
   authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
+  projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
   appId: process.env.REACT_APP_APPID
